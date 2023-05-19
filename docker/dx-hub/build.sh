625e2d657ee89bbd1f001fee4ef4b59313438b84
Olivier Liechti <olivier.liechti@wasabi-tech.com>
2023-05-19 09:24:36 +0200

    Upgrade to Backstage 1.14.0

--- docker/dx-hub/build.sh
+++ docker/dx-hub/build.sh
@@ -5,6 +5,6 @@ yarn install --frozen-lockfile
 yarn tsc
 yarn build:backend --config ../../app-config.yaml ../../app-config.docker.yaml
 
-docker build --file ../docker/dx-hub/Dockerfile -t avaliasystems/dx-hub .
-# docker buildx build --platform linux/amd64,linux/arm64 --push -t avaliasystems/dx-hub .
+# docker build --file ../docker/dx-hub/Dockerfile -t avaliasystems/dx-hub .
+docker buildx build --file ../docker/dx-hub/Dockerfile --platform linux/amd64,linux/arm64 --push -t avaliasystems/dx-hub .
 
