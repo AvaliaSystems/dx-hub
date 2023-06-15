4b8c747ca10e61ccd7a6cfb31d6b82ad277f2987
Olivier Liechti <olivier.liechti@wasabi-tech.com>
2023-06-15 17:08:00 +0200

    Fix tsc issue

--- dx-hub/plugins/simple/src/components/ExampleFetchComponent/ExampleFetchComponent.test.tsx
+++ dx-hub/plugins/simple/src/components/ExampleFetchComponent/ExampleFetchComponent.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import { ExampleFetchComponent } from './ExampleFetchComponent';
 import { rest } from 'msw';
 import { setupServer } from 'msw/node';
